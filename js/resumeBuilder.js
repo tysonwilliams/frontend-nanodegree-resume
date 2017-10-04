e9b261aaed3bab90d16a9a240acbb10d94b568fd
Tyson Williams <tyson.j.williams@gmail.com>
2017-10-04 12:48:57 -0600

    Clean up older comments and console.logs

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,14 +2,8 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-//$("#main").append("Tyson Williams");
-
+const newEmail = "tyson.j.williams@gmail.com";
 const awesomeThoughts = "I am Tyson Williams and I am AWESOME!";
-
-console.log(awesomeThoughts);
-
 let funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-
-console.log(funThoughts);
 
 $("#main").append(funThoughts);
